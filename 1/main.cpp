747de08909f87fb16c8ef24ade867d1c9cbec8ab
Mr.Geekman <bunin260200@mail.ru>
2021-03-13 20:36:39 +0300

    Added solution without GTest

--- 1/main.cpp
+++ 1/main.cpp
@@ -2,45 +2,74 @@
 #include "typelist.hpp"
 
 // Тестирование
-struct Test1 {
-    using CharList = TypeList<char, signed char, unsigned char>;
+bool TestCreation() {
+    using CharList = TypeListBuilder<char, signed char, unsigned char>::Result;
     using FirstElem = typename TypeAt<CharList, 0u>::Result;
     using SecondElem = typename TypeAt<CharList, 1u>::Result;
     using ThirdElem = typename TypeAt<CharList, 2u>::Result;
-    static const bool result = (
+    bool result = (
             std::is_same<FirstElem, char>::value
             && std::is_same<SecondElem, signed char>::value
             && std::is_same<ThirdElem, unsigned char>::value
     );
-};
+    return result;
+}
 
-struct Test2 {
-    using List = TypeList<int, char, float>;
-    // Sanity check
-    using Elem = typename List::Head;
-    static const bool sanity = std::is_same<Elem, int>::value;
-    // Test
-    enum {int_index = IndexOf<List, int>::value};
-    enum {char_index = IndexOf<List, char>::value};
-    enum {float_index = IndexOf<List, float>::value};
-    static const bool result_1 = (int_index == -1);
-    static const bool result_2 = (char_index == 1);
-    static const bool result_3 = (float_index == -1);
-    static const bool result = result_1 && result_2 && result_3;
-};
+bool TestEmpty() {
+    using EmptyList = TypeListBuilder<>::Result;
+    using EraseResult = Erase<EmptyList, char>::Result;
+    bool result = std::is_same<EraseResult, EmptyList>::value;
+    return result;
+}
 
-// добавить тесты на удаление
-// удаление в пустом списке
-// удаление если объект на превом месте
-// удаление если объект где-то в середине
-// удаление если объекта нет
-// удаление если две копии объекта
+bool TestFirst() {
+    using CharList = TypeListBuilder<char, signed char, unsigned char>::Result;
+    using CorrectResult = TypeListBuilder<signed char, unsigned char>::Result;
+    using EraseResult = Erase<CharList, char>::Result;
+    bool result = std::is_same<EraseResult, CorrectResult>::value;
+    return result;
+}
+
+bool TestMiddle() {
+    using CharList = TypeListBuilder<char, signed char, unsigned char>::Result;
+    using CorrectResult = TypeListBuilder<char, unsigned char>::Result;
+    using EraseResult = Erase<CharList, signed char>::Result;
+    bool result = std::is_same<EraseResult, CorrectResult>::value;
+    return result;
+}
+
+bool TestLast() {
+    using CharList = TypeListBuilder<char, signed char, unsigned char>::Result;
+    using CorrectResult = TypeListBuilder<char, signed char>::Result;
+    using EraseResult = Erase<CharList, unsigned char>::Result;
+    bool result = std::is_same<EraseResult, CorrectResult>::value;
+    return result;
+}
+
+bool TestNoObj() {
+    using CharList = TypeListBuilder<char, signed char, unsigned char>::Result;
+    using EraseResult = Erase<CharList, int>::Result;
+    bool result = std::is_same<EraseResult, CharList>::value;
+    return result;
+}
+
+bool TestTwoObj() {
+    using CharList = TypeListBuilder<char, signed char, char, unsigned char>::Result;
+    using CorrectResult = TypeListBuilder<signed char, char, unsigned char>::Result;
+    using EraseResult = Erase<CharList, char>::Result;
+    bool result = std::is_same<EraseResult, CorrectResult>::value;
+    return result;
+}
+
 
 void testAll() {
-    std::cout << "Test 1: " << Test1::result << std::endl;
-    std::cout << "Test 2 Sanity: " << Test2::sanity << std::endl;
-    std::cout << "Test 2: " << Test2::result << std::endl;
-    // ...
+    std::cout << "Test 1: " << TestCreation() << std::endl;
+    std::cout << "Test 2: " << TestEmpty() << std::endl;
+    std::cout << "Test 3: " << TestFirst() << std::endl;
+    std::cout << "Test 4: " << TestMiddle() << std::endl;
+    std::cout << "Test 5: " << TestLast() << std::endl;
+    std::cout << "Test 6: " << TestNoObj() << std::endl;
+    std::cout << "Test 7: " << TestTwoObj() << std::endl;
 }
 
 
--- 1/typelist.hpp
+++ 1/typelist.hpp
@@ -1,65 +1,53 @@
-#ifndef INC_1_TYPELIST_CPP
-#define INC_1_TYPELIST_CPP
+#ifndef INC_1_TYPELIST_HPP
+#define INC_1_TYPELIST_HPP
 
 // Определение списка типов
 class NullType{};
 
-template<typename T=NullType, typename ... U>
+template<typename T, typename U>
 struct TypeList {
     using Head = T;
-    using Tail = TypeList<U ...>;
+    using Tail = U;
 };
 
-template<typename T>
-struct TypeList<T> {
-    using Head = T;
-    using Tail = NullType;
+// Создание списка типов
+template<typename... Ts>
+struct TypeListBuilder {
+    using Result = NullType;
+};
+
+template <typename T, typename... Ts>
+struct TypeListBuilder<T, Ts...> {
+    using Result = TypeList<T, typename TypeListBuilder<Ts...>::Result>;
 };
 
 // Индексирование списка типов
-template<class TList, unsigned int i>
+
+template <class TList, unsigned int i>
 struct TypeAt {
     using Result = typename TypeAt<typename TList::Tail, i-1>::Result;
 };
 
-template<class TList>
-struct TypeAt<TList, 0u> {
-    using Result = typename TList::Head;
+template <class Head, class Tail>
+struct TypeAt<TypeList<Head, Tail>, 0> {
+    using Result = Head;
 };
 
-// Поиск индекса первого вхождения
-template<class TList, class T>
-struct IndexOf {
-private:
-    enum {temp = IndexOf<typename TList::Tail, T>::value};
-public:
-    enum {value = (temp == -1) ? -1 : temp + 1};
-};
-
-template<class T>
-struct IndexOf<NullType, T> {
-    enum {value = -1};
-};
-
-template<class T, class Tail>
-struct IndexOf<TypeList<T, Tail>, T> {
-    enum {value = 0};
-};
-
-// Удаление первого вхождения по значению
-template<class TList, class T>
+// Удаление элемента из списка типов
+template <typename TList, typename T>
 struct Erase {
     using Result = TypeList<typename TList::Head, typename Erase<typename TList::Tail, T>::Result>;
 };
 
-template<class T>
+template <typename T>
 struct Erase<NullType, T> {
     using Result = NullType;
 };
 
-template<class T, class Tail>
+template <typename T, typename Tail>
 struct Erase<TypeList<T, Tail>, T> {
     using Result = Tail;
 };
 
-#endif //INC_1_TYPELIST_CPP
+
+#endif //INC_1_TYPELIST_HPP
